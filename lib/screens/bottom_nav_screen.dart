8d98e8c72b8ed4b91d71d7e8e83dcd43ec9556f9
Mohit140306 <mohitjagini@gmail.com>
2024-06-27 13:25:55 +0530

    adding bottom nav

---
+++ lib/screens/bottom_nav_screen.dart
@@ -0,0 +1,60 @@
+// lib/screens/bottom_nav_screen.dart
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'cart_screen.dart';
+import 'favorite_screen.dart';
+import 'profile_screen.dart';
+
+class BottomNavScreen extends StatefulWidget {
+  @override
+  _BottomNavScreenState createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    CartScreen(),
+    FavoriteScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white, // Set background color of bottom navigation bar
+        unselectedItemColor: Colors.grey, // Set color of unselected items
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue, // Set color of selected item
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
