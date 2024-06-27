bfd54a992a935ff0be874f2dce371cebdd70ae8c
Mohit140306 <mohitjagini@gmail.com>
2024-06-27 13:22:40 +0530

    adding modules

---
+++ lib/models/product.dart
@@ -0,0 +1,19 @@
+// lib/models/product.dart
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  final String category;
+  int quantity;
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    required this.quantity,
+    required this.category,
+  });
+}
